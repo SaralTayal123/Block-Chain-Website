7e8b42b789e13cbbb2e7606494344dd692429365
Saral Tayal <thunderbird211@gmail.com>
2020-02-15 13:23:24 -0500

    web2

---
+++ web/flutter_service_worker.js
@@ -0,0 +1,40 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/assets\AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"/assets\FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets\LICENSE": "9e8760f5de9816c5610a25315d421de9",
+"/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/index.html": "33503b49da5777cd3dc848dfe3fec3d1",
+"/main.dart.js": "4c9be24becad5269b0afd393d0df7f5b",
+"/manifest.json": "b11f134c5ce27533d67e6ab1d8670296"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request, {
+          credentials: 'include'
+        });
+      })
+  );
+});
